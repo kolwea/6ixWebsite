6b9ed564fa139004cba84b7d30698e60946cf292
Kolbe Weathington <kswof97@yahoo.com>
2023-05-15 12:48:10 -0500

    Simple Login Card

---
+++ app/login/page.tsx
@@ -0,0 +1,54 @@
+'use client';
+import { Container, Card, Row, Input, Spacer, Button, Grid, styled } from "@nextui-org/react";
+import React from "react";
+
+// NextUI only works in client-side components!!!
+
+const LoginCard = () => {
+    return (
+        <Container justify="center">
+            <Card css={{ height: "300px", }}>
+                <Card.Header css={{ pd: 'none' }}>
+                    <img
+                        alt="nextui logo"
+                        src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+                        width="34px"
+                        height="34px"
+                    />
+                    <Grid.Container css={{ pl: "$6" }}>
+                        <Grid xs={12}>
+                            6ixDegrees
+                        </Grid>
+                        <Grid xs={12}>
+                            itsuptwo.us
+                        </Grid>
+                    </Grid.Container>
+                </Card.Header>
+                <Card.Divider />
+                <Card.Body css={{ pd: 'none' }}>
+                    <Spacer y={1} />
+                    <Row justify="center" align="center">
+                        <Input width="300px" size="lg" color="secondary" clearable bordered labelPlaceholder="Username" placeholder="Username" />
+                    </Row>
+                    <Spacer y={2} />
+                    <Row justify="center" align="center">
+                        <Input.Password width="300px" size="lg" color="secondary" clearable bordered labelPlaceholder="Password" placeholder="Password" />
+                    </Row>
+                </Card.Body>
+                <Card.Footer css={{ pd: 'none' }} >
+                    <Container display="flex" justify="center" css={{ margin: 'auto' }}>
+                        <Button>Login</Button>
+                    </Container>
+                </Card.Footer>
+            </Card>
+        </Container >)
+
+}
+
+export default function LoginPage() {
+    return (
+        <Container display="flex" alignContent="center" justify="center" css={{ height: '100vh', mw: '500px' }}>
+            <LoginCard />
+        </Container>);
+}
+
---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import { NextUIProvider } from '@nextui-org/react'
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>);
+}
