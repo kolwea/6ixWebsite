01802bea49daa3176f0e1b74aa9aafc5091d2739
Kolbe Weathington <kswof97@yahoo.com>
2023-05-16 22:51:49 -0500

    Try adding fonts

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,10 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
       },
+      {
+        test: /\.(woff|woff2|ttf|eot)$/,
+        use: 'file?name=fonts/[name].[ext]!static'
+    },
 
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
