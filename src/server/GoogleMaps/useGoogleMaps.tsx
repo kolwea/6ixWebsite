93f4215b2769a0b67a6ad55cabd8a3a7769bcd6e
Kolbe Weathington <kswof97@yahoo.com>
2023-05-22 17:22:40 -0500

    ...

--- src/server/GoogleMaps/useGoogleMaps.tsx
+++ src/server/GoogleMaps/useGoogleMaps.tsx
@@ -3,7 +3,6 @@ import { Loader } from "@googlemaps/js-api-loader";
 const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""
 
 export const useGoogleMaps = () => {
-    // console.log(process.env.GOOGLE_MAPS_API_KEY)
     const loader = new Loader({
         apiKey,
         version: "weekly",
