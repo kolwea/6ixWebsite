43f9eca1ad00e6932274efe380a0aa7001ec35ef
Kolbe Weathington <kswof97@yahoo.com>
2023-05-22 16:11:54 -0500

    ...

--- app/dashboard/GoogleMaps/useGoogleMaps.tsx
+++ app/dashboard/GoogleMaps/useGoogleMaps.tsx
@@ -4,12 +4,10 @@ import styles from './styles.module.scss'
 
 
 export const GoogleMap = () => {
-    const [scriptLoaded, setScriptLoaded] = useState(false);
     const [mapOptions, setMapOptions] = useState<google.maps.MapOptions>({
         center: { lat: -34.397, lng: 150.644 },
         zoom: 8,
     })
-    const [map, setMap] = useState<google.maps.Map>();
 
     const loader = new Loader({
         apiKey: "AIzaSyCHfrmurbBUCNNmsOs-NeEjQr_kVkwXys8",
@@ -19,7 +17,6 @@ export const GoogleMap = () => {
 
     useEffect(() => {
         loader.load().then(async () => {
-            setScriptLoaded(true);
             console.log("Google maps loaded.")
             const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
             const mapWrapper = document.getElementById('mapWrapper') as HTMLElement
